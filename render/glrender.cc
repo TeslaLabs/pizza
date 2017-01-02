e56dc3bd38d6c64a80ee632a6c82e886ee3456d3
Jeffery Stager <jffry09@gmail.com>
2017-01-02 21:44:12 +0900

    Fixed Linux version not displaying anything

--- render/glrender.cc
+++ render/glrender.cc
@@ -38,7 +38,7 @@ GLRender::~GLRender() {
 }
 
 void GLRender::Update() {
-    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     ErrorCheck("glclear");
 }
 
